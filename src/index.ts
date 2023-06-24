f76feec3dcd3e29515608cb1f511e1b96babde71
LukeMcCann <developer-lukemccann@outlook.com>
2023-06-24 21:36:10 +0100

    Remove deprecated firstLoad

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ import Particles from './enums/Particles';
 const simulation = new CanvasSimulation({ containerId: 'canvas' });
 simulation.initialize();
 
-let numParticles = Particles.INITIAL_PARTICLE_NUM;
 let velocityDivisor = Particles.INITIAL_VELOCITY_DIVISOR;
 let speed = Particles.INITIAL_SPEED;
 let particles = [];
@@ -18,41 +17,13 @@ const formHandler = new FormHandler(Form.SIMCONTROLS_NAME);
 const particleInput = document.getElementById(Form.SIMCONTROLS.PARTICLEINPUT) as HTMLInputElement;
 const speedInput = document.getElementById(Form.SIMCONTROLS.SPEEDINPUT) as HTMLInputElement;
 
-let isFirstLoad = true;
-
-const generateParticles = ({ speed }) => {
+const generateParticles = () => {
   const desiredNumParticles = parseInt(particleInput.value, 10) || Particles.INITIAL_PARTICLE_NUM;
 
   if (desiredNumParticles > particles.length) {
     generateAdditionalParticles({ particles, desiredNumParticles });
   } else if (desiredNumParticles < particles.length) {
     removeParticles({ particles, desiredNumParticles });
-  }
-
-  if (isFirstLoad) {
-    generateInitialParticles({ particles, numParticles });
-    isFirstLoad = false;
-  }
-};
-
-const generateInitialParticles = ({ particles, numParticles }) => {
-  let i = particles.length;
-  while (i < numParticles && i < simulation.MAX_ITER) {
-    particles.push(
-      new Particle({
-        ctx: simulation.ctx!,
-        x: random_int_from_interval(0, simulation.canvas.width),
-        y: random_int_from_interval(0, simulation.canvas.height),
-        radius: 10,
-        color: random_hex_color_code(),
-        speed: speed,
-        velocityX: random_int_from_interval(-2, 2),
-        velocityY: random_int_from_interval(-2, 2),
-        velocityXMutator: velocityDivisor,
-        velocityYMutator: velocityDivisor,
-      })
-    );
-    i++;
   }
 };
 
@@ -85,14 +56,14 @@ const removeParticles = ({ particles, desiredNumParticles }) => {
 
 particleInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
-    generateParticles({ speed });
+    generateParticles();
   }
 });
 
 speedInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     speed = parseFloat(speedInput.value) || Particles.INITIAL_SPEED;
-    generateParticles({ speed });
+    generateParticles();
   }
 });
 
@@ -100,7 +71,7 @@ formHandler.setInputValue(Form.SIMCONTROLS.PARTICLEINPUT, Particles.INITIAL_PART
 formHandler.setInputValue(Form.SIMCONTROLS.VELOCITYINPUT, velocityDivisor.toString());
 formHandler.setInputValue(Form.SIMCONTROLS.SPEEDINPUT, Particles.INITIAL_SPEED.toString());
 
-generateParticles({ speed });
+generateParticles();
 
 const animate = () => {
   simulation.clearCanvas();
